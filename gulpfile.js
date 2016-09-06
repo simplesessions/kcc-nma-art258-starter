e221b12c881d01d621359e7bb48704c453ae9e1d
root <root@playground.simplesessions.com>
2016-09-06 06:07:03 -0400

    Starter project files

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const browserSync = require('browser-sync')
+const gulp        = require('gulp')
+const concat      = require('gulp-concat')
+const sass        = require('gulp-sass')
+
+// compile sass
+gulp.task('sass', () => {
+    return gulp.src('./src/**/*.scss').
+        pipe(sass()).
+        pipe(gulp.dest('./dist/style.css')).
+        pipe(browserSync.reload({ stream: true }))
+})
+
+// run browser-sync
+gulp.task('sync', () => {
+    browserSync.init('./', {
+        server: {
+            baseDir: './dist/'
+        }
+    })
+})
+
+// copy over your HTML files
+gulp.task('html', () => {
+    return gulp.src('./src/**/*.html').
+        pipe(gulp.dest('./dist/')).
+        pipe(browserSync.reload({ stream: true }))
+})
+
+// copy over all your images
+gulp.task('images', () => {
+    return gulp.src('./src/images/**/*.{gif,jpg,png}').
+        pipe(gulp.dest('./dist/images/')).
+        pipe(browserSync.reload({ stream: true }))
+})
+
+// build everything
+gulp.task('build', ['sass', 'html', 'images'])
+
+// start watching all your files
+gulp.task('watch', ['build', 'sync'], () => {
+    gulp.watch('./src/scss/**/*.scss', ['sass'])
+    gulp.watch('./src/**/*.html', ['html'])
+    gulp.watch('./src/**/*.{gif,jpg,png}', ['images'])
+})
+
+gulp.task('default', ['watch'])
+
